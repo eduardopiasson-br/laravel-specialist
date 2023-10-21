eab37991d095e0cf64555fff3ec3a504796b8587
Eduardo Piasson <eduardopiasson7@gmail.com>
2023-10-21 15:44:28 -0300

    Factories + Seeders

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -19,5 +20,27 @@ class Plan extends Model
 
     public function signatures() {
         return $this->hasMany(Signature::class);
+    }
+
+    public function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => ucfirst($value)
+        );
+    }
+
+    public function cod(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => strtoupper($value),
+            set: fn($value) => strtolower($value)
+        );
+    }
+
+    public function fullname(): Attribute
+    {
+        return Attribute::make(
+            get: fn($values, $attributes) => $attributes['cod'] . ' - ' . $attributes['name']
+        );
     }
 }
